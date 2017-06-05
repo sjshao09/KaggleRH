095004b423a63a351336f030d1e894725045b236
Patrick Shao <sjshao09@gmail.com>
2017-06-05 18:15:36 +0100

    log naive xgb, .1.4.5 + 0.968 => 0.256/0.312/0.318

--- naive_xgb.py
+++ naive_xgb.py
@@ -4,19 +4,115 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import model_selection, preprocessing
 import xgboost as xgb
+import geopandas as gpd
+from pyproj import Proj, transform, Geod
+
 
 # ----------------- Settings ----------------- #
 EN_CROSSVALIDATION   = True
 EN_IMPORTANCE        = True
 DEFAULT_TRAIN_ROUNDS = 384
 
-
-
-
 # ----------------- Read Data ----------------- #
 df      = pd.read_csv('input/train.csv')
 test_df = pd.read_csv('input/test.csv')
 macro   = pd.read_csv('input/macro.csv')
+shp     = gpd.read_file('input/moscow_adm.shp')
+
+# ----------------- Data Cleaning ----------------- #
+# Training Set
+df.loc[df.id==13549, 'life_sq'] = 74
+df.loc[df.id==10092, 'build_year'] = 2007
+df.loc[df.id==10092, 'state'] = 3
+df.loc[df.id==13120, 'build_year'] = 1970
+df.loc[df.id==25943, 'max_floor'] = 17
+# Clean - Full Sq
+df = df[(df.full_sq>1)|(df.life_sq>1)]
+df.loc[(df.full_sq<10) & (df.life_sq>1), 'full_sq'] = df.life_sq
+df = df[df.full_sq<400]
+# Clean - Life Sq
+df.loc[df.life_sq > df.full_sq*4, 'life_sq'] = df.life_sq/10
+df.loc[df.life_sq > df.full_sq, 'life_sq'] = np.nan
+df.loc[df.life_sq < 2, 'life_sq'] = np.nan
+df.loc[df.life_sq < df.full_sq * 0.3, 'life_sq'] = np.nan
+# Clean - Kitch Sq
+df.loc[df.kitch_sq < 2, 'kitch_sq'] = np.nan
+df.loc[df.kitch_sq > df.full_sq * 0.5, 'kitch_sq'] = np.nan
+# Clean - Build Year
+df.loc[df.build_year<1000, 'build_year'] = np.nan
+df.loc[df.build_year>2050, 'build_year'] = np.nan
+# Clean - Num Room
+df.loc[df.num_room<1, 'num_room'] = np.nan
+df.loc[(df.num_room>9) & (df.full_sq<100), 'num_room'] = np.nan
+# Clean - Floor and Max Floor
+df.loc[df.floor==0, 'floor'] = np.nan
+df.loc[df.max_floor==0, 'max_floor'] = np.nan
+df.loc[(df.max_floor==1) & (df.floor>1), 'max_floor'] = np.nan
+df.loc[df.max_floor>50, 'max_floor'] = np.nan
+df.loc[df.floor>df.max_floor, 'floor'] = np.nan
+
+# Test Set
+test_df.loc[test_df.id==30938,  'full_sq'] = 37.8
+test_df.loc[test_df.id==35857,  'full_sq'] = 42.07
+test_df.loc[test_df.id==35108,  'full_sq'] = 40.3
+test_df.loc[test_df.id==33648,  'num_room'] = 1
+# Clean - Full Sq
+test_df.loc[(test_df.full_sq<10) & (test_df.life_sq>1), 'full_sq'] = test_df.life_sq
+# Clean - Life Sq
+test_df.loc[test_df.life_sq>test_df.full_sq*2, 'life_sq'] = test_df.life_sq/10
+test_df.loc[test_df.life_sq > test_df.full_sq, 'life_sq'] = np.nan
+test_df.loc[test_df.life_sq < 2, 'life_sq'] = np.nan
+test_df.loc[test_df.life_sq < test_df.full_sq * 0.3, 'life_sq'] = np.nan
+# Clean - Kitch Sq
+test_df.loc[test_df.kitch_sq < 2, 'kitch_sq'] = np.nan
+test_df.loc[test_df.kitch_sq > test_df.full_sq * 0.5, 'kitch_sq'] = np.nan
+# Clean - Build Year
+test_df.loc[test_df.build_year<1000, 'build_year'] = np.nan
+test_df.loc[test_df.build_year>2050, 'build_year'] = np.nan
+# Clean - Num Room
+test_df.loc[test_df.num_room<1, 'num_room'] = np.nan
+test_df.loc[(test_df.num_room>9) & (test_df.full_sq<100), 'num_room'] = np.nan
+# Clean - Floor and Max Floor
+test_df.loc[test_df.floor==0, 'floor'] = np.nan
+test_df.loc[test_df.max_floor==0, 'max_floor'] = np.nan
+test_df.loc[(test_df.max_floor==1) & (test_df.floor>1), 'max_floor'] = np.nan
+test_df.loc[test_df.max_floor>50, 'max_floor'] = np.nan
+test_df.loc[test_df.floor>test_df.max_floor, 'floor'] = np.nan
+
+# ----------------- New Features ----------------- #
+# Auxiliary Feature - price/sq
+df['price/sq'] = df['price_doc'] / df['full_sq']
+# New Feature - bad_floor
+df['bad_floor'] = (df['floor']==1) | (df['floor']==df['max_floor'])
+test_df['bad_floor'] = (test_df['floor']==1) | (test_df['floor']==test_df['max_floor'])
+# New Feature - kitch_sq/full_sq
+df["kitch_sq/full_sq"] = df["kitch_sq"] / df["full_sq"]
+test_df["kitch_sq/full_sq"] = test_df["kitch_sq"] / test_df["full_sq"]
+# log size
+df["full_sq"] = np.log(df["full_sq"])
+df["life_sq"] = np.log(df["life_sq"])
+df["kitch_sq"] = np.log(df["kitch_sq"])
+test_df["full_sq"] = np.log(test_df["full_sq"])
+test_df["life_sq"] = np.log(test_df["life_sq"])
+test_df["kitch_sq"] = np.log(test_df["kitch_sq"])
+
+
+# ----------------- Remove Extreme Data ----------------- #
+RANDOM_SEED_SPLIT = 1
+df_1m = df[ (df.price_doc<=1000000) & (df.product_type=="Investment") ]
+df    = df.drop(df_1m.index)
+df_1m = df_1m.sample(frac=0.1, replace=False, random_state=RANDOM_SEED_SPLIT)
+
+df_2m = df[ (df.price_doc==2000000) & (df.product_type=="Investment") ]
+df    = df.drop(df_2m.index)
+df_2m = df_2m.sample(frac=0.4, replace=False, random_state=RANDOM_SEED_SPLIT)
+
+df_3m = df[ (df.price_doc==3000000) & (df.product_type=="Investment") ]
+df    = df.drop(df_3m.index)
+df_3m = df_3m.sample(frac=0.5, replace=False, random_state=RANDOM_SEED_SPLIT)
+
+df    = pd.concat([df, df_1m, df_2m, df_3m])
+
 
 # Plot Original Data Set
 OrigTrainValidSetFig = plt.figure()
@@ -24,61 +120,11 @@ ax1 = plt.subplot(311)
 plt.hist(np.log1p(df['price_doc'].values), bins=200, color='b')
 plt.title('Original Data Set')
 
-'''
-# ----------------- Data Cleaning Essential ----------------- #
-# Training Set
-df = df[(df.full_sq>1)|(df.life_sq>1)]
-df = df[(df.full_sq<500)]
-df.loc[df.id==13549, 'life_sq'] = 74
-df.loc[df.id==10092, 'build_year'] = 2007
-df.loc[df.id==10092, 'state'] = 3
-df.loc[df.id==25943, 'max_floor'] = 17
-df.loc[df.build_year>2050, 'build_year'] = np.nan
-df.loc[(df.full_sq<10) & (df.life_sq>1), 'full_sq'] = df.life_sq
-df.loc[df.life_sq>df.full_sq*2, 'life_sq'] = df.life_sq/10
-# Test Set
-test_df.loc[test_df.id==30938,  'full_sq'] = 37.8
-test_df.loc[test_df.id==35857,  'full_sq'] = 42.07
-test_df.loc[test_df.id==35108,  'full_sq'] = 40.3
-test_df.loc[test_df.life_sq>test_df.full_sq*2, 'life_sq'] = test_df.life_sq/10
-'''
-
-'''
-# ----------------- Data Cleaning Enhanced ----------------- #
-# Data Cleaning - Training Set
-# full_sq
-# life_sq
-df.loc[(df.life_sq<1)|(df.life_sq>df.full_sq), 'life_sq'] = 1
-# kitch_sq
-df.loc[df.kitch_sq<2, 'kitch_sq'] = 1
-# build_year
-df.loc[(df.build_year<1000), 'build_year'] = np.nan
-# num_room
-df.loc[df.full_sq<30, 'num_room'] = 1
-df.loc[df.num_room<1, 'num_room'] = np.nan
-df.loc[(df.num_room>9) & (df.full_sq<100), 'num_room'] = np.nan
-# floor
-df.loc[df.floor==0, 'floor'] = np.nan
-# max_floor
-df.loc[(df.max_floor==0)|(df.max_floor<df.floor), 'max_floor'] = np.nan
-
-# Data Cleaning - Test Set
-# life_sq
-test_df.loc[test_df.life_sq<1, 'life_sq']  = np.nan
-# kitch_sq
-test_df.loc[(test_df.kitch_sq>test_df.full_sq*0.7)|(test_df.kitch_sq<1) , 'kitch_sq'] = 1
-# build_year
-test_df.loc[(test_df.build_year<1000) | (test_df.build_year>2050), 'build_year'] = np.nan
-# num_room
-test_df.loc[test_df.id==33648,  'num_room'] = 1
-# max_floor
-test_df.loc[test_df.max_floor<test_df.floor, 'max_floor'] = np.nan
-'''
 
 
 # ----------------- Training Data ----------------- #
-y_train = df["price_doc"] * 0.968 + 10
-x_train = df.drop(["id", "timestamp", "price_doc"], axis=1)
+y_train = np.log1p(df['price_doc'] * 0.97)
+x_train = df.drop(["id", "timestamp", "price_doc", "price/sq"], axis=1)
 # Encoding
 for c in x_train.columns:
     if x_train[c].dtype == 'object':
@@ -118,7 +164,7 @@ xgb_params = {
 # ----------------- Cross Validation ----------------- #
 if EN_CROSSVALIDATION:
     print "[INFO] Cross Validation..."
-    cv_output = xgb.cv(xgb_params, dtrain, num_boost_round=1000, early_stopping_rounds=20,
+    cv_output = xgb.cv(xgb_params, dtrain, num_boost_round=1000, early_stopping_rounds=10,
                    verbose_eval=10, show_stdv=True)
     DEFAULT_TRAIN_ROUNDS = len(cv_output)
     print "[INFO] Optimal Training Rounds =", DEFAULT_TRAIN_ROUNDS
@@ -127,14 +173,14 @@ if EN_CROSSVALIDATION:
 print "[INFO] Training for", DEFAULT_TRAIN_ROUNDS, "rounds..."
 model      = xgb.train(xgb_params, dtrain, num_boost_round=DEFAULT_TRAIN_ROUNDS, 
                        evals=[(dtrain, 'train')], verbose_eval=10)
-y_predict  = model.predict(dtest)
+y_predict  = np.expm1(model.predict(dtest))
 submission = pd.DataFrame({'id': test_df.id, 'price_doc': y_predict})
 submission.to_csv('submission.csv', index=False)
 print submission.head()
 
 # Plot Original, Training and Test Sets
 ax4 = plt.subplot(312, sharex=ax1)
-plt.hist(np.log1p(y_train), bins=200, color='b')
+plt.hist(y_train, bins=200, color='b')
 #plt.setp(ax4.get_xticklabels(), visible=False)
 plt.title('Training Data Set')
 plt.subplot(313, sharex=ax1)
