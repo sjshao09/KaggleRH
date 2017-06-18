498f13f8287087fd1a091365c86f0f9646facac9
Patrick Shao <sjshao09@gmail.com>
2017-06-18 12:45:09 +0100

    svr{ep=0.23, C=32, gamma=0.0002}:0.31879, avg721

--- svr.py
+++ svr.py
@@ -130,28 +130,12 @@ test_df["life_sq"] = np.log(test_df["life_sq"])
 test_df["kitch_sq"] = np.log(test_df["kitch_sq"])
 
 
-# ----------------- Fill by median ----------------- #
+# ----------------- Handle Missing Data ----------------- #
+# Fill by Mean
 df.fillna(df.median(axis=0), inplace=True)
 test_df['product_type'].fillna(test_df['product_type'].mode().iloc[0], inplace=True)
 test_df.fillna(df.median(axis=0), inplace=True)
 
-'''
-# ----------------- Remove Extreme Data ----------------- #
-RANDOM_SEED_SPLIT = 1
-df_1m = df[ (df.price_doc<=1000000) & (df.product_type=="Investment") ]
-df    = df.drop(df_1m.index)
-df_1m = df_1m.sample(frac=0.1, replace=False, random_state=RANDOM_SEED_SPLIT)
-
-df_2m = df[ (df.price_doc==2000000) & (df.product_type=="Investment") ]
-df    = df.drop(df_2m.index)
-df_2m = df_2m.sample(frac=0.33, replace=False, random_state=RANDOM_SEED_SPLIT)
-
-df_3m = df[ (df.price_doc==3000000) & (df.product_type=="Investment") ]
-df    = df.drop(df_3m.index)
-df_3m = df_3m.sample(frac=0.5, replace=False, random_state=RANDOM_SEED_SPLIT)
-
-df    = pd.concat([df, df_1m, df_2m, df_3m])
-'''
 
 # Plot Original Data Set
 OrigTrainValidSetFig = plt.figure()
@@ -190,9 +174,9 @@ x_test  = scaler.transform(x_test)
 if EN_CROSSVALIDATION:
     print "[INFO] Cross Validation..."
     model = SVR(kernel='rbf', tol=0.001, cache_size=4096)
-    param_dist = {'gamma': [1e-4, 1e-3, 1e-2, 1e-1, 1, 2, 4, 8, 16, 32, 64],
-                  'C': [1, 2, 4, 8, 16, 32, 64, 128, 256, 512],
-                  'epsilon': [1e-3, 1e-2, 1e-1, 1, 2, 4],
+    param_dist = {'gamma': [0.0001, 0.0002, 0.0003, 0.0004, 0.0005],
+                  'C': [8, 16, 24, 32, 40, 48, 56, 64, 72, 80],
+                  'epsilon': [0.20, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29],
                  }
     # Scoring: 'neg_mean_squared_error', 'neg_mean_squared_log_error'
     r = RandomizedSearchCV(model, param_distributions=param_dist, scoring='neg_mean_squared_error', 
@@ -202,40 +186,22 @@ if EN_CROSSVALIDATION:
     print ('Best parameters: {}'.format(r.best_params_))
     top_parameters = pd.DataFrame(r.cv_results_)
     top_parameters = top_parameters[['mean_test_score', 'std_test_score', 'params']].sort_values(by='mean_test_score', ascending=False)
-    print top_parameters.head(10)
+    print top_parameters.head(16)
 
 '''
-Best score: -0.299571819032
-Best parameters: {'epsilon': 0.1, 'C': 32, 'gamma': 0.001}
-     mean_test_score  std_test_score  \
-67         -0.299572        0.069265   
-92         -0.300596        0.072400   
-43         -0.302187        0.068818   
-79         -0.305577        0.072888   
-51         -0.308141        0.074275   
-93         -0.308918        0.074618   
-34         -0.308936        0.074403   
-121        -0.310386        0.073459   
-103        -0.310528        0.068592   
-30         -0.311433        0.074532   
-
-                                               params  
-67       {u'epsilon': 0.1, u'C': 32, u'gamma': 0.001}  
-92     {u'epsilon': 0.1, u'C': 256, u'gamma': 0.0001}  
-43         {u'epsilon': 0.1, u'C': 1, u'gamma': 0.01}  
-79     {u'epsilon': 0.001, u'C': 16, u'gamma': 0.001}  
-51   {u'epsilon': 0.001, u'C': 128, u'gamma': 0.0001}  
-93      {u'epsilon': 0.001, u'C': 1, u'gamma': 0.001}  
-34    {u'epsilon': 0.001, u'C': 64, u'gamma': 0.0001}  
-121    {u'epsilon': 0.01, u'C': 16, u'gamma': 0.0001}  
-103       {u'epsilon': 0.01, u'C': 2, u'gamma': 0.01}  
-30       {u'epsilon': 0.1, u'C': 1, u'gamma': 0.0001}  
-
+   mean_test_score  std_test_score   params
+5        -0.279929        0.059918   {u'epsilon': 0.25, u'C': 32, u'gamma': 0.0002} => 0.31931, avg712
+4        -0.280964        0.061110   {u'epsilon': 0.24, u'C': 32, u'gamma': 0.0002} => 0.31901, avg717
+3        -0.281964        0.062060   {u'epsilon': 0.23, u'C': 32, u'gamma': 0.0002} => 0.31879, avg721
+2        -0.283140        0.063244   {u'epsilon': 0.22, u'C': 32, u'gamma': 0.0002} => 0.31888, avg726
+1        -0.284553        0.064547   {u'epsilon': 0.21, u'C': 32, u'gamma': 0.0002} => 0.31892, avg729
+0        -0.285949        0.065621   {u'epsilon': 0.20, u'C': 32, u'gamma': 0.0002} => 0.31891, avg733
+0        -0.287327        0.066543   {u'epsilon': 0.19, u'C': 32, u'gamma': 0.0002} => 0.31899, avg738  
 '''
 
 # ----------------- Training ----------------- #
 print "[INFO] Training..."
-model = SVR(kernel='rbf', epsilon=0.1, C=256, gamma=0.0001, tol=0.001, cache_size=16384, verbose=2)
+model = SVR(kernel='rbf', epsilon=0.23, C=32, gamma=0.0002, tol=0.001, cache_size=16384, verbose=2)
 model.fit(x_train, y_train)
 print "[INFO] Predicting..."
 y_predict  = np.expm1(model.predict(x_test))
